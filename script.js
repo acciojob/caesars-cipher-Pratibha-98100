dc7121412de8514b65a1c97e05c3222d235533ce
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-05-21 22:10:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,12 +12,20 @@ const lookup = {
 
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  
+	// code
+	for( var i=0; i<encodedStr.length; i++){
+		var c= encodedStr.charcodeAt(i);
 
-  return ;//return decodedArr
+		// shifting the alphabtes
+		if(c >= 65 && c <= 90){
+			decodedArr.push(String.fromCharcode(c.toUpperCase()));
+		}
+	}
+
+  return decodedArr;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(rot13("SERR YBIR? NPPVBWBO"));
 
