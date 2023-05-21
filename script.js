0e23ea494a0232690abda09148ced002af718449
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-05-22 00:51:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Your Script here.
-
 const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
@@ -9,27 +7,21 @@ const lookup = {
   'U': 'H','V': 'I','W': 'J','X': 'K',
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
-
+ 
 function rot13(encodedStr){
+   const words = encodedStr.split(" ");  // String to Array
    let decodedArr = []; // Your Result goes here
-  
-	// code
-	for( var i=0; i<encodedStr.length; i++){
-		var c= encodedStr.charcodeAt(i);
-
-		// shifting the alphabtes
-		if(c >= 65 && c <= 90){
-			decodedArr.push(String.fromCharcode(c.toUpperCase()));
-		}
-		else {
-			decodedArr.push(encodedStr.charAt(i));  // non-alphabetic character
-		}
-	}
-
-  return decodedArr.join("");
+  // Only change code below this line
+   for(let i=0; i< words.length; i++){
+        const word = words[i]
+        let decoded_word  = ''
+        for(let j = 0; j < word.length; j++) {
+            const char = word.charAt(j)
+            const decoded_char = lookup[char]
+            decoded_word += decoded_char
+        }
+        decodedArr.push(decoded_word)
+     }
+        return decodedArr.join(" ");
 }
-
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
 module.exports = rot13;
